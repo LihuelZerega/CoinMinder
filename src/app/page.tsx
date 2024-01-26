7c8c11f29ddccc8651b5e569f3ff0aed59f750ce
LihuelZerega <zeregalihuel@gmail.com>
2024-01-26 16:23:42 -0300

    LandingPage Background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,47 @@ export default function Home() {
   return (
     <NextUIProvider>
       <NavBarConrtrollerMenu />
-      <main className="mx-6 my-6 sm:mx-8 md:mx-12 lg:mx-32 xl:mx-48 2xl:mx-64">
-        <HeroSection />
-        <section className="flex flex-col xl:flex-row border-b-1">
-          <div className="w-full xl:w-2/3">
-            <FinancialNews />
-          </div>
-          <div className="w-full xl:w-1/3">
-            <MarketSummary />
-          </div>
-        </section>
-      </main>
+
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#fcd535] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+
+        <main className="mx-6 my-6 sm:mx-8 md:mx-12 lg:mx-32 xl:mx-48 2xl:mx-64">
+          <HeroSection />
+          {/* <section className="flex flex-col xl:flex-row border-b-1">
+            <div className="w-full xl:w-2/3">
+              <FinancialNews />
+            </div>
+            <div className="w-full xl:w-1/3">
+              <MarketSummary />
+            </div>
+          </section> */}
+        </main>
+
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#fcd535] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+      </div>
+
       <Footer />
     </NextUIProvider>
   );
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,60 +1,49 @@
 "use client"
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from 'react'
 
-interface QuoteData {
-  c: number; // Current price
-  d: number; // Change
-  dp: number; // Percent change
-  h: number; // High price of the day
-  l: number; // Low price of the day
-  o: number; // Open price of the day
-  pc: number; // Previous close price
-}
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
 
-function StockQuote() {
-  const [quoteData, setQuoteData] = useState<QuoteData | null>(null);
-  const symbol = "TSLA"; // Symbol for Tesla
-
-  useEffect(() => {
-    const fetchQuoteData = async () => {
-      try {
-        const apiKey = "cmpuqdhr01ql684rolagcmpuqdhr01ql684rolb0";
-        const response = await axios.get(
-          `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apiKey}`
-        );
-
-        setQuoteData(response.data);
-      } catch (error) {
-        console.error(`Error fetching quote data for ${symbol}:`, error);
-      }
-    };
-
-    fetchQuoteData();
-  }, []);
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="border p-4 rounded-md">
-      <h1 className="text-lg font-semibold mb-3">{symbol} Quote</h1>
-      {quoteData ? (
-        <div>
-          <p>Current Price: ${quoteData.c.toFixed(2)}</p>
-          {quoteData.d !== null && (
-            <p>Change: ${quoteData.d.toFixed(2)}</p>
-          )}
-          {quoteData.dp !== null && (
-            <p>Percent Change: {quoteData.dp.toFixed(2)}%</p>
-          )}
-          <p>High Price of the Day: ${quoteData.h.toFixed(2)}</p>
-          <p>Low Price of the Day: ${quoteData.l.toFixed(2)}</p>
-          <p>Open Price of the Day: ${quoteData.o.toFixed(2)}</p>
-          <p>Previous Close Price: ${quoteData.pc.toFixed(2)}</p>
-        </div>
-      ) : (
-        <p>Loading quote data...</p>
-      )}
-    </div>
-  );
-}
+    <div className="bg-white">
 
-export default StockQuote;
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+
+
+
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        
+      </div>
+    </div>
+  )
+}
