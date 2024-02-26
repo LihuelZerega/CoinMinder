610262d32d6cc639e95064bf153b99f38ad17d35
LihuelZerega <zeregalihuel@gmail.com>
2024-02-26 15:19:40 -0300

    Bitcoin charts and descriptions

--- src/pages/Crypto/Crypto.tsx
+++ src/pages/Crypto/Crypto.tsx
@@ -5,7 +5,6 @@ import Footer from "@/components/Footer";
 import Highlights from "./Highlights";
 import CryptoPricesTable from "@/pages/Crypto/CryptoPricesTable";
 import TopTokensColapsablePanel from "./TopTokensColapsablePanel";
-import Image from "next/image";
 
 function Crypto() {
   return (
@@ -13,9 +12,6 @@ function Crypto() {
       <NavBar />
       <main className="mx-auto max-w-7xl py-6 lg:px-8 mt-24 mb-24">
         <section className="flex flex-row items-center space-x-4 border-b-1 pb-6 mb-6 mx-6">
-          {/* <div>
-            <img src="https://res.cloudinary.com/dszjgdktf/image/upload/v1706544546/cwxrrvhlipgwfub2homc.png" width={100} height={100} alt="" />
-          </div> */}
           <div>
             <h1 className="mb-4 text-lg font-light text-[#38bdf8]">Crypto</h1>
             <h1 className="font-medium text-4xl">Today&apos;s Crypto Prices</h1>
---
+++ src/pages/Crypto/CryptoCharts/BitcoinChart24hs.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const BitcoinChart24hs: React.FC = () => {
+  const [chartData, setChartData] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchBitcoinChart24hsData();
+  }, []);
+
+  const fetchBitcoinChart24hsData = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/crypto/bitcoin/chart');
+      if (!response.ok) {
+        throw new Error('Failed to fetch Bitcoin chart data');
+      }
+      const data = await response.json();
+      const formattedData = data.map((entry: any) => ({
+        ...entry,
+        timestamp: new Date(Number(entry.timestamp)).toLocaleDateString(), 
+      }));
+      const last24hsData = formattedData.slice(-2);
+      setChartData(last24hsData);
+    } catch (error) {
+      console.error('Error fetching Bitcoin chart data:', error);
+    }
+  };
+
+  return (
+    <div style={{ width: '100%', height: 300 }}>
+      <ResponsiveContainer>
+        <AreaChart
+          data={chartData}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timestamp" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="price" stroke="#38bdf8" fill="#38bdf8" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default BitcoinChart24hs;
